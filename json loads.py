214fac2c4f8833c733360590aa0faec464127182
keerthana <keerthana@sequoiaat.com>
2024-09-18 23:24:58 +0530

    added the readme file

--- json loads.py
+++
@@ -1,4 +0,0 @@
-import json
-x='[{"name": "keerthana","age":2, "language":"english"}]'
-y=json.loads(x)
-print(y)
--- selective_item_print.py
+++
@@ -1,5 +0,0 @@
-import json
-x='[{"name": "keerthana","age":2, "language":"english"},{"name":"sathana","age":19}]'
-y=json.loads(x)
-for item in y:
-    print(item['name'],item['age'])
